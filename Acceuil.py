a3769b6233aea41dd67a12961f166659f1b73b9a
Theo Massenya <theo.massenya3@gmail.com>
2025-09-02 21:57:52 +0200

    ajout de la base principal du projet

---
+++ Acceuil.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import datetime
+
+
+
+
+st.set_page_config(
+    page_title="Page Principale",
+    page_icon="🏠"   
+)
+
+
+st.title("Bienvenue 👋")
+st.write("vous retrouverez ici le menu principal, ainsi que votre agenda.")
+
---
+++ pages/Notes.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Deuxième Page",
+    page_icon="📝"
+)
+
+
+st.title("Bienvenue sur la deuxième page")
+st.write("Contenu de la deuxième page ici…")
